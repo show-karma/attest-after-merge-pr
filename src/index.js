0a5afa421f259ff0486c529a0410a28762ff419d
Arthh <arthur.ulhoa28@gmail.com>
2023-10-23 19:03:52 -0300

    feat: 16

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 console.log("attest lines added and removed");
 console.log("added 3");
 console.log("added 4");
-console.log("added 5");
 console.log("added 6");
 console.log("added 7");
 console.log("added 8");
